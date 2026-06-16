743a43071506984c18295c42c6a61c91818ab733
diego <diego@localhost.localdomain>
2026-06-16 15:29:06 -0300

    fixed some stuff

--- scripts/db-updates.sh
+++ scripts/db-updates.sh
@@ -1,5 +1,18 @@
-#!/bin/bash 
-up=$(checkupdates | wc -l)
-echo $up  > /tmp/updates
+#!/bin/sh
+. /etc/os-release
+
+case "$ID" in
+	arch)
+		up=$(checkupdates 2>/dev/null | wc -l)
+		;;
+	opensuse*|opensuse-tumbleweed|opensuse-leap)
+		up=$(zypper lu 2>/dev/null | grep -c '|')
+		;;
+	*)
+		up="?"
+		;;
+esac
+
 color=$(get_color "*color7")
-echo "^c$color^"   $(cat /tmp/updates) "^d^"
+
+printf "^c%s^  %s ^d^\n" "$color" "$up"
